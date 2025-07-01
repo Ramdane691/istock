3d7b6b26df8b73620acaad214a115ae23d45aeb0
Ramdane AMAOUZ <ramdane.amaouz@etu.univ-lyon1.fr>
2025-07-01 15:27:26 +0200

    mm

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -61,7 +61,7 @@ function Register() {
     }
 
     try {
-      const response = await fetch("https://istock-backend-p2uc.onrender.com", {
+      const response = await fetch("https://istock-backend-p2uc.onrender.com/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
